11e94648a79e658c2533363fe2e2d0a6dde2abcb
thaitienvan <vantienthai95@gmail.com>
2022-06-05 22:51:14 +0700

    add group

--- main.go
+++ main.go
@@ -10,11 +10,19 @@ import (
 func main() {
 	router := gin.Default()
 	router.Use(connection.PointToDB)
-	router.GET("/students", controller.GetStudent)
-	router.GET("/student/:id", controller.GetStudentById)
-	router.POST("/student", controller.PostStudent)
-	router.GET("/searchstudent/:fullname", controller.SearchStudent)
-	router.DELETE("/student/:id", controller.DeleteStudent)
-	router.GET("/student/searchbyyear/:year", controller.FindStudentByYear)
+	// router.GET("/students", controller.GetStudent)
+	// router.GET("/student/:id", controller.GetStudentById)
+	// router.POST("/student", controller.PostStudent)
+	// router.GET("/searchstudent/:fullname", controller.SearchStudent)
+	// router.DELETE("/student/:id", controller.DeleteStudent)
+	st := router.Group("/student")
+	{
+		st.GET("/", controller.GetStudent)
+		st.GET("/:id", controller.GetStudentById)
+		st.POST("/", controller.PostStudent)
+		st.GET("/search/:fullname", controller.SearchStudent)
+		st.DELETE("/:id", controller.DeleteStudent)
+		st.GET("/searchbyyear/:year", controller.FindStudentByYear)
+	}
 	router.Run("localhost:8111")
 }
